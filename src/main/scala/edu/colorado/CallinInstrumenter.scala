7b414e8d31ba10dc88572495b9423479bdd092ff
shawn meier <shawn.meier@colorado.edu>
2016-10-10 14:47:52 -0600

    added code that adds instrumentation class

--- src/main/scala/edu/colorado/CallinInstrumenter.scala
+++ src/main/scala/edu/colorado/CallinInstrumenter.scala
@@ -3,8 +3,9 @@ package edu.colorado
 import java.util
 
 import edu.colorad.cs.TraceRunner.Config
-import soot.jimple.{InvokeStmt, AbstractStmtSwitch}
-import soot.{SootMethod, PatchingChain, Body, BodyTransformer}
+import soot.jimple.{AbstractStmtSwitch, InvokeStmt}
+import soot.{Body, BodyTransformer, PatchingChain, Scene, SootMethod}
+
 import scala.collection.JavaConversions._
 import scala.util.matching.Regex
 
@@ -12,12 +13,26 @@ import scala.util.matching.Regex
  * Created by s on 9/21/16.
  */
 class CallinInstrumenter(config: Config) extends BodyTransformer{
+  var runFirst = false //TODO: somehow run once code can be run multiple times, fix this
 
   val applicationPackages = config.applicationPackages.map((a:String) =>{
     Utils.packageGlobToSignatureMatchingRegex(a).r
   })
 
   override def internalTransform(b: Body, phaseName: String, options: util.Map[String, String]): Unit = {
+    if(!runFirst) {
+      val inst = new InstrumentationGenerators()
+      inst.addInstrumentationClasses()
+      runFirst = true
+    }
+    //TODO: remove the following
+    //TODO: I can find the inst class in the following but it does not end up in the resulting jar?
+//    Scene.v().getClasses.map(a =>
+//      {
+//        val name: String = a.getName
+//        val _ = 2
+//      })
+    //TODO: end remove
 
 
     val declaration: String = b.getMethod.getDeclaration
