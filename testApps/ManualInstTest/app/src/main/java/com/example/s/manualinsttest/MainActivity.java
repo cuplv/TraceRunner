42d72edc6098a89712be3aef3b5d8ed0d87da63b
shawn meier <shawn.meier@colorado.edu>
2016-10-11 18:37:47 -0600

    App for testing instrumentation outside of soot

---
+++ testApps/ManualInstTest/app/src/main/java/com/example/s/manualinsttest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.s.manualinsttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.colorado.plv.tracerunner_runtime_instrumentation.TraceRunnerRuntimeInstrumentation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TraceRunnerRuntimeInstrumentation.logCallin("hi!");
+        setContentView(R.layout.activity_main);
+    }
+}
