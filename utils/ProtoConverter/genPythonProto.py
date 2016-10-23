3efb32c36de778cb244bacc94001b326a6d7ddb2
Shawn <shawn.meier@colorado.edu>
2016-10-23 13:26:44 -0600

    gen protos bug fix

--- utils/ProtoConverter/genPythonProto.py
+++ utils/ProtoConverter/genPythonProto.py
@@ -3,10 +3,12 @@ import os
 import subprocess
 
 def compileProto():
-    dirname = os.path.dirname(__file__)
-
+    # dirname = os.path.dirname(__file__)
+    dirname = os.path.dirname(os.path.abspath(__file__))
     split = dirname.split("/")
     projRoot = "/".join(split[0:-2])
+    print "----"
+    print projRoot
     srcDir = projRoot + "/TraceRunnerRuntimeInstrumentation/tracerunnerinstrumentation/src/main/proto/edu/colorado/plv/tracerunner_runtime_instrumentation/"
     protoFile = "tracemsg.proto"
 
