8b35060f5eff3764acd6d6c2f91dbc77cedfbb00
Shawn <shawn.meier@colorado.edu>
2016-10-24 16:46:29 -0600

    work on callback instrumenter

--- src/main/scala/edu/colorado/CallbackInstrumenter.scala
+++ src/main/scala/edu/colorado/CallbackInstrumenter.scala
@@ -3,7 +3,9 @@ package edu.colorado
 import java.util
 
 import edu.colorad.cs.TraceRunner.Config
-import soot.{Body, BodyTransformer}
+import soot.{Body, BodyTransformer, PatchingChain, SootMethod}
+
+import scala.util.matching.Regex
 
 /**
   * Created by s on 10/24/16.
@@ -13,6 +15,26 @@ class CallbackInstrumenter(config: Config, instrumentationClasses: scala.collect
     Utils.packageGlobToSignatureMatchingRegex(a).r
   })
   override def internalTransform(b: Body, phaseName: String, options: util.Map[String, String]) = {
-    
+    val declaration: String = b.getMethod.getDeclaration
+    val method: SootMethod = b.getMethod()
+
+    val name: String = b.getMethod.getName
+    //val signature: String = b.getMethod.getSignature
+    val signature: String = b.getMethod.getDeclaringClass.getName
+
+    val matches: Boolean = applicationPackages.exists((r: Regex )=>{
+      signature match{
+        case r() => true
+        case _ => false
+      }
+    })
+    if(matches && name != "<clinit>") {
+      val units: PatchingChain[soot.Unit] = b.getUnits;
+
+      units.addFirst(???)
+
+
+      units.addLast(???)
+    }
   }
 }
