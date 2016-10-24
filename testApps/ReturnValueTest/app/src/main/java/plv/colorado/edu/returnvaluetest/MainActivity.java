025c9e35d6018b35582be741b5bcf9c09529fad2
Shawn <shawn.meier@colorado.edu>
2016-10-24 15:49:56 -0600

    app for testing return value logging

---
+++ testApps/ReturnValueTest/app/src/main/java/plv/colorado/edu/returnvaluetest/MainActivity.java
@@ -0,0 +1,18 @@
+package plv.colorado.edu.returnvaluetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getApplication(); //test return value from non used return
+        TextView tv = (TextView)findViewById(R.id.h);
+        tv.setText("goodbye world!");
+    }
+}
