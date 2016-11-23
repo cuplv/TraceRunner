52de71c9dd99696cadfa2fa137294865b35fc9ed
Soon Lee Edmund Lam <edmund.lam@colorado.edu>
2016-11-23 16:12:55 -0700

    now allows multiple auto tracing scripts per app.

--- autoTracer/autoLaunch.py
+++ autoTracer/autoLaunch.py
@@ -15,22 +15,7 @@ def dirName(path):
    (stdout, error) = p.communicate()
    return stdout.replace('\n','')
 
-if __name__ == "__main__":
-   if len(sys.argv) != 5:
-      print "usage: python autoLaunch.py <Path to (Resigned) Instrumented APK> <Path to (Resigned) Auto Tracer APK> <Auto Tracer Class Name> <Path to Output Trace ProtoBuf File>"
-      sys.exit(1)
-
-   instrumented_apk_path = sys.argv[1]
-   auto_tracer_apk_path  = sys.argv[2]
-   auto_tracer_name      = sys.argv[3]
-   output_proto_path     = sys.argv[4]
-
-   appName,activityName = getAPKInfo(instrumented_apk_path)
-   tracerName,_ = getAPKInfo(auto_tracer_apk_path)
-
-   print "Instrumented App Package Name: %s" % appName
-   print "Instrumented App Activity Name: %s" % activityName
-   print "Auto Tracer Name: %s" % tracerName
+def runAutoTracer(appPackageName, atracerPackageName, atracerClassName, outputProtoPath):
 
    print "Starting ADB+NetCat Bridge @ 5050..."
    adb_proc = Popen(['adb','reverse','tcp:5050', 'tcp:5050'], stdout=PIPE)
@@ -38,36 +23,56 @@ if __name__ == "__main__":
    nc_proc = Popen(['nc','-l','-p','5050'], stdout=PIPE)
    print "Started ADB+NetCat Bridge"
 
-   print "Uninstalling Previous Version of Instrumented App..."
-   adb_proc = Popen(['adb','uninstall',appName], stdout=PIPE)
-   outcome,_ = adb_proc.communicate()
-   print "Uninstall Completed: %s" % outcome
-
-   print "Uninstalling Previous Version of Auto Tracer..."
-   adb_proc = Popen(['adb','uninstall',tracerName], stdout=PIPE)
-   outcome,_ = adb_proc.communicate()
-   print "Uninstall Completed: %s" % outcome
-
-   print "Installing Current Version of Instrumented App..."
-   adb_proc = Popen(['adb','install',instrumented_apk_path], stdout=PIPE)
-   outcome,_ = adb_proc.communicate()
-   print "Install Completed: %s" % outcome
-
-   print "Installing Current Version of Auto Tracer..."
-   adb_proc = Popen(['adb','install',auto_tracer_apk_path], stdout=PIPE)
-   outcome,_ = adb_proc.communicate()
-   print "Install Completed: %s" % outcome
-
-
    print "Running Auto Tracer on Instrumented App..."
-   trace_proc = Popen(['adb','shell','am','instrument','-w','-r','-e','debug','false','-e','class','%s.%s' % (appName,auto_tracer_name),'%s/android.support.test.runner.AndroidJUnitRunner' % tracerName], stdout=PIPE)
+   trace_proc = Popen(['adb','shell','am','instrument','-w','-r','-e','debug','false','-e','class','%s.%s' % (appPackageName,atracerClassName),'%s/android.support.test.runner.AndroidJUnitRunner' % atracerPackageName], stdout=PIPE)
    outcome,error = trace_proc.communicate()
    print "Trace Completed: %s, %s" % (outcome,error)
 
    trace,_ = nc_proc.communicate()
 
-   with open(output_proto_path, "w") as f:
+   with open("%s/%s.out" % (outputProtoPath,atracerClassName), "w") as f:
       f.write(trace)
+
+if __name__ == "__main__":
+   if len(sys.argv) != 5:
+      print "usage: python autoLaunch.py <Path to (Resigned) Instrumented APK> <Path to (Resigned) Auto Tracer APK> <Auto Tracer Class Names (Colon Separated)> <Path to Output Folder>"
+      sys.exit(1)
+
+   instrumentedAPKPath = sys.argv[1]
+   atracerAPKPath      = sys.argv[2]
+   atracerClassNames   = sys.argv[3]
+   outputProtoPath     = sys.argv[4]
+
+   appPackageName,activityName = getAPKInfo(instrumentedAPKPath)
+   atracerPackageName,_ = getAPKInfo(atracerAPKPath)
+
+   print "Instrumented App Package Name: %s" % appPackageName
+   print "Instrumented App Activity Name: %s" % activityName
+   print "Auto Tracer Package Name: %s" % atracerPackageName
+
+   print "Uninstalling Previous Version of Instrumented App..."
+   adb_proc = Popen(['adb','uninstall',appPackageName], stdout=PIPE)
+   outcome,_ = adb_proc.communicate()
+   print "Uninstall Completed: %s" % outcome
+
+   print "Uninstalling Previous Version of Auto Tracer..."
+   adb_proc = Popen(['adb','uninstall',atracerPackageName], stdout=PIPE)
+   outcome,_ = adb_proc.communicate()
+   print "Uninstall Completed: %s" % outcome
+
+   print "Installing Current Version of Instrumented App..."
+   adb_proc = Popen(['adb','install',instrumentedAPKPath], stdout=PIPE)
+   outcome,_ = adb_proc.communicate()
+   print "Install Completed: %s" % outcome
+
+   print "Installing Current Version of Auto Tracer..."
+   adb_proc = Popen(['adb','install',atracerAPKPath], stdout=PIPE)
+   outcome,_ = adb_proc.communicate()
+   print "Install Completed: %s" % outcome
+
+   for atracerClassName in atracerClassNames.split(":"):
+       print "Running Auto Tracing for %s" % atracerClassName
+       runAutoTracer(appPackageName, atracerPackageName, atracerClassName, outputProtoPath)
 
    print "All Done!"
 
