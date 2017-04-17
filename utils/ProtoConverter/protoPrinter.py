f67f80e95b5bafb1b07eecfa0f47b66e1dda51ea
Shawn <shawn.meier@colorado.edu>
2017-04-17 11:41:08 -0600

    script to truncate traces at missing messages

--- utils/ProtoConverter/protoPrinter.py
+++ utils/ProtoConverter/protoPrinter.py
@@ -3,14 +3,17 @@ import google.protobuf.internal.decoder as decoder
 
 import argparse
 
+
 def printFromFile(file):
     pbufs, err = decodeProtobuf(file)
     for pbuf in pbufs:
         print pbuf
 
+
 def checkFromFile(file, print_p):
-    pbufs,err = decodeProtobuf(file)
+    pbufs, err = decodeProtobuf(file)
     last = 0
+    missing = 0
     for pbuf in pbufs:
         if pbuf.msg.message_id == last:
             last += 1
@@ -19,10 +22,14 @@ def checkFromFile(file, print_p):
         else:
             if print_p:
                 print pbuf
-            print "---missing: " + str(pbuf.msg.message_id - last)
-            break
+            # print "---missing: " + str(pbuf.msg.message_id - last)
+            missing += pbuf.msg.message_id - last
+            last = pbuf.msg.message_id
+    return missing
+
+
 def decodeProtobuf(file):
-    f = open(file,'rb')
+    f = open(file, 'rb')
     data = f.read()
     dataBegin = 0
     pbufList = []
@@ -30,31 +37,31 @@ def decodeProtobuf(file):
     error = None
     while True:
         if dataBegin >= dataLen:
-            return pbufList,None
+            return pbufList, None
         try:
             (size, position) = decoder._DecodeVarint(data, dataBegin)
         except Exception as e:
             error = e
             break
-	try:
-        	pbuf = tracemsg_pb2.TraceMsgContainer()
-        	pbuf.ParseFromString(data[position:position + size])
-        	dataBegin = size + position
-        	pbufList.append(pbuf)
-	except Exception as e:
-		error = e
-		break
-    return pbufList,error
+        try:
+            pbuf = tracemsg_pb2.TraceMsgContainer()
+            pbuf.ParseFromString(data[position:position + size])
+            dataBegin = size + position
+            pbufList.append(pbuf)
+        except Exception as e:
+            error = e
+            break
+    return pbufList, error
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Print human readable data from protobuf trace')
     parser.add_argument('--trace', type=str,
-                        help="file with protobuf data encoded by \"writeDelimitedTo\"",required=True)
+                        help="file with protobuf data encoded by \"writeDelimitedTo\"", required=True)
     parser.add_argument('--check', help="check for missing messages")
     args = parser.parse_args()
 
     if not args.check:
-    	printFromFile(args.trace)
+        printFromFile(args.trace)
     else:
-        checkFromFile(args.trace,True)
+        print str(checkFromFile(args.trace, False)) + "@" + args.trace
