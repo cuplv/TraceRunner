01d4473c26eff8ff09b16b7d98856341a02a20bc
Shawn Meier <shawn.meier@colorado.edu>
2017-05-10 14:16:46 -0400

    application class test

--- testApps/Application_Class_Test/app/src/main/java/com/example/s/application_class_test/MainActivity.java
+++ testApps/Application_Class_Test/app/src/main/java/com/example/s/application_class_test/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String string = "3";
+        string = string + "3";
+        Integer.parseInt(string);
     }
 }
